8b82836202cfea0474e3bb03c88fb2c9b8914999
Tubix <vdmitriev556@gmail.com>
2025-11-02 17:41:08 +0000

    added manual packaging script

---
+++ package.sh
@@ -0,0 +1,4 @@
+
+cd ./bin
+
+adt -package -storetype pkcs12 -tsa http://tsa.mesign.com -keystore ./../tankseditor.p12 -target air AlternativaEditor.air AlternativaEditor-app.xml AlternativaEditor.swf
